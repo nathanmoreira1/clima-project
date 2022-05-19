3b656fc93ede07dadb0f4fec6d7494c499f70511
Nathan Moreira de Macedo <66101714+nathanmoreira1@users.noreply.github.com>
2022-05-19 18:37:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.querySelector(".busca").addEventListener("submit", async (e)=>{
+    e.preventDefault();
+
+    let input = document.querySelector("#searchInput").value;
+    
+    if (input !== '') {
+        showWarning("Carregando...");
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=d06cdb298fafc83c520d5ab677fc477e&units=metric&lang=pt_br`
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.cod === 200) {
+            showInformations({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                temp_icon: json.weather[0].icon,
+                wind_speed: json.wind.speed,
+                wind_angle: json.wind.deg
+            })
+        }
+        else {
+            document.querySelector(".resultado").style.display = "none";
+            showWarning("Não encontramos essa localização.")
+        }
+    }
+    else {
+        document.querySelector(".resultado").style.display = "none";
+    }
+
+});
+
+showWarning = (msg) => {
+    document.querySelector(".aviso").innerHTML = msg
+}
+
+showInformations = (json) => {
+    showWarning('');
+    document.querySelector(".titulo").innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector(".tempInfo").innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector(".ventoInfo").innerHTML = `${json.wind_speed} <span>km/h</span>`;
+    document.querySelector(".temp img").src = `http://openweathermap.org/img/wn/${json.temp_icon}@2x.png`;
+    document.querySelector(".ventoPonto").style.transform = `rotate(${json.wind_angle-90}deg)`;
+    document.querySelector(".resultado").style.display = "block";
+
+}

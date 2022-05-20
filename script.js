cd3a0ab20bfcdfa26f9a068a3a33fc6b0d964992
Nathan Moreira de Macedo <66101714+nathanmoreira1@users.noreply.github.com>
2022-05-20 10:01:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelector(".busca").addEventListener("submit", async (e)=>{
     if (input !== '') {
         showWarning("Carregando...");
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=d06cdb298fafc83c520d5ab677fc477e&units=metric&lang=pt_br`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=e19a41ce44154872f28c7deac0af1f12&units=metric&lang=pt_br`
 
         let results = await fetch(url);
         let json = await results.json();
